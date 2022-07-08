6e912f2eadfb8f8144791cd4d75d0f737fa2efbe
Angel  Ifechukwu Onyebuchi <angelifechukwu00@gmail.com>
2022-07-08 00:22:29 +0000

    Attempted challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers){
+  
+let sum = 0
+    for (let i=0; i<arrayOfNumbers.length; i++){
+      sum += arrayOfNumbers[i]
+    } 
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -47,9 +53,15 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let fahrenheit = [];
+  for(let i=0; i<arrayOfNumbers.length; i++ )
+    {
+      fahrenheit.push(Math.trunc((arrayOfNumbers[i] *9/5) +32));
+    }
+  
+    return fahrenheit;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
